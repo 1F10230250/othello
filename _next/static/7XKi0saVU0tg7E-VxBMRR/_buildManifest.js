03c46eaaae39f84ce528e90e0218d15bc1dbd618
1F10230250 <1F10230250@users.noreply.github.com>
2023-06-01 11:48:25 +0000

    deploy: e16dd8584ec9aab3d22a310fd0644193690aed80

--- _next/static/7XKi0saVU0tg7E-VxBMRR/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/fc51e9327dda119c.css","static/chunks/pages/index-1187ce0c84dc0aec.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
