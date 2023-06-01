b0aa338573d06f1dfb267b6a8400845304b1fdff
1F10230250 <s1f102302506@iniad.org>
2023-06-01 20:44:01 +0900

    pass追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,59 +1,135 @@
+import { useState } from 'react';
 import styles from './index.module.css';
-
 const Home = () => {
+  let [turncolor, setTurncolor] = useState(1);
+  //prettier-ignore
+  const [board, setBoard] = useState([
+  [0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0],
+  [0,0,0,1,2,0,0,0],
+  [0,0,0,2,1,0,0,0],
+  [0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0]
+  ]);
+  for (let yaa = 0; yaa <= 7; yaa += 1) {
+    for (let xaa = 0; xaa <= 7; xaa += 1) {
+      if (board[yaa][xaa] === 3) {
+        board[yaa][xaa] = 0;
+      }
+    }
+  }
+  for (let ya = 0; ya <= 7; ya += 1) {
+    for (let xa = 0; xa <= 7; xa += 1) {
+      for (let distancey = -1; distancey <= 1; distancey += 1) {
+        for (let distancex = -1; distancex <= 1; distancex += 1) {
+          if (
+            board[ya + distancey] !== undefined &&
+            board[xa + distancex] !== undefined &&
+            board[ya + distancey][xa + distancex] === 3 - turncolor
+          ) {
+            for (let distance = 2; distance <= 7; distance += 1) {
+              if (
+                board[ya + distance * distancey] !== undefined &&
+                board[xa + distance * distancex] !== undefined &&
+                board[ya + distance * distancey][xa + distance * distancex] === 0
+              ) {
+                break;
+              }
+              if (
+                board[ya + distance * distancey] !== undefined &&
+                board[xa + distance * distancex] !== undefined &&
+                board[ya + distance * distancey][xa + distance * distancex] === turncolor
+              ) {
+                if (board[ya][xa] === 0 || board[ya][xa] === 3) {
+                  board[ya][xa] = 3;
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  const click_pass = () => {
+    turncolor = 3 - turncolor;
+
+    setTurncolor(turncolor);
+
+    console.log('変わったよ', turncolor);
+  };
+
+  let stop = 100;
+  const clickcell = (x: number, y: number) => {
+    if (x === 100 && y === 100) {
+      console.log('pass!');
+    }
+    console.log(x, y);
+    const newBoard: number[][] = JSON.parse(JSON.stringify(board));
+    if ((y !== 100 && x !== 100 && board[y][x] === 0) || board[y][x] === 3) {
+      for (let distancey = -1; distancey <= 1; distancey += 1) {
+        for (let distancex = -1; distancex <= 1; distancex += 1) {
+          if (
+            board[y + distancey] !== undefined &&
+            board[x + distancex] !== undefined &&
+            board[y + distancey][x + distancex] === 3 - turncolor
+          ) {
+            for (let distance = 2; distance <= 7; distance += 1) {
+              if (stop === 50) {
+                stop = 100;
+                break;
+              }
+              if (
+                board[y + distance * distancey] !== undefined &&
+                board[x + distance * distancex] !== undefined &&
+                board[y + distance * distancey][x + distance * distancex] === 0
+              ) {
+                break;
+              }
+              if (
+                board[y + distance * distancey] !== undefined &&
+                board[x + distance * distancex] !== undefined &&
+                board[y + distance * distancey][x + distance * distancex] === turncolor
+              ) {
+                for (let turn = 0; turn <= distance; turn += 1) {
+                  newBoard[y + turn * distancey][x + turn * distancex] = turncolor;
+                  stop = 50;
+                }
+                setTurncolor(3 - turncolor);
+              }
+            }
+          }
+        }
+      }
+    }
+    setBoard(newBoard);
+  };
+
   return (
     <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code} style={{ backgroundColor: '#fafafa' }}>
-            pages/index.js
-          </code>
-        </p>
-
-        <div className={styles.grid}>
-          <a className={styles.card} href="https://nextjs.org/docs">
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a className={styles.card} href="https://nextjs.org/learn">
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a className={styles.card} href="https://github.com/vercel/next.js/tree/master/examples">
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            className={styles.card}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <img src="vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      {/* <div className={styles.pass} onClick={() => clickcell(100, 100)}>
+        <p>パス</p>
+      </div> */}
+      <div className={styles.pass} onClick={click_pass}>
+        pass
+      </div>
+      <div className={styles.board}>
+        {board.map((row, y) =>
+          row.map((color, x) => (
+            <div className={styles.cell} key={`${x}-${y}`} onClick={() => clickcell(x, y)}>
+              {color !== 0 && (
+                <div
+                  className={styles.stone}
+                  style={{ background: color === 1 ? '#000' : color === 2 ? '#fff' : '#f00' }}
+                />
+              )}
+            </div>
+          ))
+        )}
+      </div>
     </div>
   );
 };
-
 export default Home;
