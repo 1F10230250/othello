76206e725b2170afda9ca5f90d57a9c9998daf36
1F10230250 <1F10230250@users.noreply.github.com>
2023-06-01 11:45:27 +0000

    deploy: b0aa338573d06f1dfb267b6a8400845304b1fdff

---
+++ _next/static/7XKi0saVU0tg7E-VxBMRR/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/fc51e9327dda119c.css","static/chunks/pages/index-1187ce0c84dc0aec.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
