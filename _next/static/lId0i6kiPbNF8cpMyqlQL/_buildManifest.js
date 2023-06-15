cb380b4c69b18cb07e353e4974ad6676054647ec
1F10230250 <1F10230250@users.noreply.github.com>
2023-06-15 09:50:05 +0000

    deploy: 4296c545ce637b26777d0c9225e862a6e2b1409e

--- _next/static/lId0i6kiPbNF8cpMyqlQL/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/fc51e9327dda119c.css","static/chunks/pages/index-0275a562d6cf4ce7.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
