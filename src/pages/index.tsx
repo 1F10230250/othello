3318b65ac3b4b2d0b60f2f5fd6fd2cd41c2ebf82
1F10230250 <s1f102302506@iniad.org>
2023-06-22 21:41:27 +0900

    バックエンド講習

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -140,12 +140,12 @@ const Home = () => {
         <p>パス</p>
       </div> */}
       <div id="your-element-id">要素</div>
-      <div className={styles.black_number} id="result-black">
+      {/* <div className={styles.black_number} id="result-black">
         黒{black}
-      </div>
-      <div className={styles.white_number} id="result-white">
+      </div> */}
+      {/* <div className={styles.white_number} id="result-white">
         白{white}
-      </div>
+      </div> */}
       <div className={styles.pass} onClick={click_pass}>
         pass
       </div>
