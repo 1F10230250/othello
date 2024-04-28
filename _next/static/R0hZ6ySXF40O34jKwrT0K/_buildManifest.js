620c81decb0a9ca2ae693fd0dbf6bc98bda753e0
1F10230250 <1F10230250@users.noreply.github.com>
2024-04-28 14:01:40 +0000

    deploy: 5d1d365e8b523796d7b8f06dcc7f6f4aad243c2f

--- _next/static/R0hZ6ySXF40O34jKwrT0K/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/8ad03ccdeb072643.css","static/chunks/pages/index-535e32c2d26ad86a.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
